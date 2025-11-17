97c0bfc0a76cc33a788cb84238a44d574ad54c4a
Clara <10088152+claraj@users.noreply.github.com>
2025-11-17 13:12:43 -0600

    Change status display to use approved field

--- student_feedback/models.py
+++ student_feedback/models.py
@@ -11,8 +11,7 @@ class Feedback(models.Model):
         # Display no more than the first 50 characters of the text to save space in the string representation
         # The full text will be saved in the database and can be viewed in the details for an individual feedback in the admin console
         email = self.email if self.email else 'anonymous'
-        return f'Review Text: {self.text[:50]}, Date: {self.date_submitted}, email: {email}, Status: {self.STATUS_CHOICES[self.status]}'
-    
+       return f'Review Text: {self.text[:50]}, Date: {self.date_submitted}, email: {email}, Status: {self.approved}'    
     
 
 
